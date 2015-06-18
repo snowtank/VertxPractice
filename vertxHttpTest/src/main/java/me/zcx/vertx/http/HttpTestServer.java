c524f685d64f5b9d58cc3abac56e4c8c68e4a9c4
zcx2001 <zcx@qq.com>
2015-06-18 19:52:12 +0800

    完成代码
    增加数据入库功能

--- vertxHttpTest/src/main/java/me/zcx/vertx/http/HttpTestServer.java
+++ vertxHttpTest/src/main/java/me/zcx/vertx/http/HttpTestServer.java
@@ -1,6 +1,7 @@
 package me.zcx.vertx.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.core.http.HttpClient;
@@ -9,20 +10,41 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.MySQLClient;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import org.apache.commons.io.FilenameUtils;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 /**
  * Created by zcx2001 on 2015-06-17.
  */
 public class HttpTestServer extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle("me.zcx.vertx.http.HttpTestServer");
+    }
+
     @Override
     public void start() throws Exception {
+        JsonObject mySQLClientConfig = new JsonObject()
+                .put("host", "192.168.13.171")
+                .put("port", 33333)
+                .put("maxPoolSize", 10)
+                .put("username", "root")
+                .put("password", "123456")
+                .put("database", "filemanage");
+
+        AsyncSQLClient sqlClient = MySQLClient.createShared(vertx, mySQLClientConfig, "fileManageDB");
+
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
@@ -41,26 +63,63 @@ public class HttpTestServer extends AbstractVerticle {
                         try {
                             String source = urlInfo.getValue();
                             URL sourceURL = new URL(source);
-                            String ext = FilenameUtils.getExtension(sourceURL.getPath());
                             URL u = getClassLoader().getResource("");
-                            String fileName = URLDecoder.decode(u.getPath(), "utf-8")
-                                    + "/webroot/" + UUID.randomUUID().toString().replace("-", "") + "." + ext;
+
+                            String originalFileName = FilenameUtils.getName(sourceURL.getPath());
+                            String originalFileNameExt = FilenameUtils.getExtension(sourceURL.getPath());
+
+                            String fileId = UUID.randomUUID().toString().replace("-", "");
+                            String fileName = fileId + "." + originalFileNameExt;
+                            String extPath = formatter.format(LocalDate.now());
+                            String savePath = URLDecoder.decode(u.getPath(), "utf-8") + "/webroot/" + extPath + "/";
+                            createDir(savePath);
+
+                            list.add(new JsonObject()
+                                    .put("state", "SUCCESS")
+                                    .put("title", fileName)
+                                    .put("source", source)
+                                    .put("url", "http://192.168.13.31:8080/static/" + extPath + "/" + fileName)
+                                    .put("size", 0));
+
                             HttpClient client = vertx.createHttpClient();
                             client.getNow(sourceURL.getPort() != -1 ? sourceURL.getPort() : sourceURL.getDefaultPort(),
                                     sourceURL.getHost(),
                                     sourceURL.getPath(),
                                     resp1 -> resp1.bodyHandler(a -> {
                                         try {
-                                            AsyncFile output = vertx.fileSystem().openBlocking(fileName, new OpenOptions());
+                                            AsyncFile output = vertx.fileSystem().openBlocking(savePath + fileName, new OpenOptions());
                                             output.write(a);
                                             output.close();
 
-                                            list.add(new JsonObject()
-                                                    .put("state", "SUCCESS")
-                                                    .put("title", FilenameUtils.getName(fileName))
-                                                    .put("source", source)
-                                                    .put("url", "http://192.168.13.31:8080/static/" + FilenameUtils.getName(fileName))
-                                                    .put("size", a.length()));
+                                            sqlClient.getConnection(c -> {
+                                                if (c.succeeded()) {
+                                                    SQLConnection connection = c.result();
+
+                                                    JsonArray params = new JsonArray();
+                                                    params.add(extPath)
+                                                            .add(fileId)
+                                                            .add(fileName)
+                                                            .add(originalFileName);
+                                                    connection.updateWithParams("INSERT INTO FileInfo (\n" +
+                                                                    "\textPath,\n" +
+                                                                    "\tfileId,\n" +
+                                                                    "\tfileName,\n" +
+                                                                    "\tfileState,\n" +
+                                                                    "\tfileTempId,\n" +
+                                                                    "\tfileType,\n" +
+                                                                    "\toriginalFileName,\n" +
+                                                                    "\tuploadDateTime,\n" +
+                                                                    "\tversion\n" +
+                                                                    ")\n" +
+                                                                    "VALUES\n" +
+                                                                    "\t(?,?,?,0,null,1,?,now(),0)", params,
+                                                            r -> {
+                                                                System.out.println("sql insert = " + r.succeeded());
+                                                                if (r.failed())
+                                                                    r.cause().printStackTrace();
+                                                            });
+                                                }
+                                            });
                                         } catch (Exception e) {
                                             e.printStackTrace();
                                         }
@@ -95,6 +154,14 @@ public class HttpTestServer extends AbstractVerticle {
     @Override
     public void stop() throws Exception {
         super.stop();
+    }
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+
+    private void createDir(String creaDir) {
+        File directory = new File(creaDir);
+        if (!directory.exists())
+            directory.mkdirs();
     }
 
     private ClassLoader getClassLoader() {
